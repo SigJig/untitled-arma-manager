a4b276a21b5d32465e4799723594a44da782bc8d
SigJig <sigmundklaa@outlook.com>
2020-03-21 17:42:47 +0100

    auto-install extdb3

--- sandbox.py
+++ sandbox.py
@@ -1,20 +1,23 @@
 
-import os, sys
+import os, sys, shutil, requests, io, platform
 from pathlib import Path, PurePath
 from manager import Builder, SteamCMD, ArmaClient
+from pyunpack import Archive
 
 DEV_DIR = Path('F:/Development')
 OUT_DIR = DEV_DIR.joinpath('Testing', 'arma-manager-test')
 
 SERVER_DIR = OUT_DIR.joinpath('server')
 INSTALL_DIR = SERVER_DIR.joinpath('arma')
+MODS_DIR = INSTALL_DIR.joinpath('mods')
 
 SOURCE_DIR = DEV_DIR.joinpath('Clones', 'phoenixrp-altislife')
 CACHE_BASE_DIR = OUT_DIR.joinpath('cache')
 
 CACHE = {
     'missions': CACHE_BASE_DIR.joinpath('missions'),
-    'mods': CACHE_BASE_DIR.joinpath('mods')
+    'mods': CACHE_BASE_DIR.joinpath('mods'),
+    'downloads': CACHE_BASE_DIR.joinpath('downloads')
 }
 
 mission_builder = Builder({
@@ -41,6 +44,38 @@ life_builder = Builder({
     }
 })
 
+def install_extdb3():
+    # note: 7zip executable is required on path
+    print('Installing extDB3...')
+    r = requests.get('https://bitbucket.org/torndeco/extdb3/downloads/extDB3-1031.7z', stream=True)
+    print(r.status_code)
+
+    dl_path = CACHE['downloads'].joinpath('extdb3.7z')
+    extract_path = CACHE['mods'].joinpath('extDB3')
+
+    for i in (CACHE['downloads'], extract_path):
+        if not i.exists():
+            os.makedirs(i)
+
+    with open(dl_path, 'wb') as fp:
+        for chunk in r.iter_content(16 * 1024):
+            fp.write(chunk)
+
+    Archive(dl_path).extractall(extract_path)
+
+    extdb3_path = MODS_DIR.joinpath('@extDB3')
+
+    if extdb3_path.exists(): shutil.rmtree(extdb3_path)
+
+    os.symlink(extract_path.joinpath('@extDB3'), extdb3_path)
+
+    for i in ('tbbmalloc.dll', 'tbbmalloc_x64.dll'):
+        src, dst = extract_path.joinpath(i), INSTALL_DIR.joinpath(i)
+
+        if dst.exists(): os.remove(dst)
+
+        os.symlink(src, dst)
+
 def build():
     print('Building mission...')
     mission_builder.build()
@@ -65,7 +100,7 @@ def run():
         config='config.cfg',
         profiles='profiles',
         mods={
-            'dir': INSTALL_DIR.joinpath('mods'),
+            'dir': MODS_DIR,
             'load': ['@life_server',  '@extDB3']
         }
     ).run()
@@ -99,6 +134,7 @@ for i in sys.argv[1:]:
     else: args.append(i)
 
 if 'i' in flags: install()
+if 'ie' in flags: install_extdb3()
 if 'b' in flags: build()
 if 'l' in flags: create_symlink()
 if 'r' in flags: run()
