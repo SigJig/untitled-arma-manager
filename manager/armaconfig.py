e0c9553bc1b71989e9b0e223aafaf3e65019f3e8
sigjig <sigmundklaa@outlook.com>
2020-03-29 21:56:01 +0200

    Added value parsing, classes to represent a3class/a3property

--- manager/armaconfig.py
+++ manager/armaconfig.py
@@ -14,8 +14,47 @@ def is_identifier(string):
 
     return all(x.isalnum() or x == '_' for x in string)
 
+class A3Class:
+    def __init__(self, name, inherits, body):
+        self.name = name
+        self.inherits = inherits
+        self.body = body
+
+    def __repr__(self):
+        return f'<{type(self).__name__} -> {self.name} : {self.inherits} {{ {self.body} }}>'
+
+class A3Property:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = self._process_value(value)
+
+    def _process_value(self, value):
+        if isinstance(value, list):
+            return [self._process_value(x) for x in value]
+        else:
+            value = value.strip()
+
+            if value and value[0] == '"':
+                if value[-1] != '"':
+                    raise Exception('Fuck you ' + value)
+
+                value = value[1:len(value) - 1]
+
+            try:
+                new_val = float(value)
+
+                if new_val.is_integer():
+                    new_val = int(new_val)
+
+                return new_val
+            except ValueError:
+                return value
+
+    def __repr__(self):
+        return f'<{type(self).__name__} -> {self.name} = {self.value}>'
+
 class Parser:
-    def __init__(self, stream, buf_len=1024):
+    def __init__(self, stream, buf_len=2048):
         self._stream = stream
         self._buf_len = buf_len
         self._cursor = 0
@@ -105,8 +144,38 @@ class Parser:
 
         return data
 
-    def _parse_value(self, char=None):
-        return self._get_while(is_identifier, char=char)
+    def _parse_string(self, char=None):
+        next_char = char or self._get(1, include_ws=True)
+        seq = ''
+
+        while True:
+            if next_char == '"':
+                if self._peek(1) == '"':
+                    seq += self._get(1, include_ws=True)
+                else:
+                    return seq + next_char
+            else:
+                seq += next_char
+
+            next_char = self._get(1, include_ws=True)
+
+    def _parse_value(self, is_array=False, char=None):
+        special_chars = [';', ',', '}'] if is_array else [';']
+
+        next_char = char or self._get(1, include_ws=True)
+        seq = ''
+
+        while next_char not in special_chars:
+            if next_char == '"':
+                seq += '"' + self._parse_string()
+            else:
+                seq += next_char
+
+            next_char = self._get(1, include_ws=True)
+
+        self._move_cursor(-1)
+
+        return seq
 
     def _parse_array(self, char=None):
         next_char = char or self._get(1)
@@ -122,12 +191,12 @@ class Parser:
             if next_char == '{':
                 body.append(self._parse_array(char=next_char))
             else:
-                body.append(self._parse_value(char=next_char))
+                body.append(self._parse_value(is_array=True, char=next_char))
 
             next_char = self._get(1)
 
-            if next_char not in ('}', ','):
-                raise InvalidCharacterException(expected=['}', ','], got=next_char)
+            if next_char not in ('}', ',', ';'):
+                raise InvalidCharacterException(expected=['}', ',', ';'], got=next_char)
 
         return body
 
@@ -170,11 +239,11 @@ class Parser:
         if char == 'c' and self._peek(4) == 'lass':
             self._move_cursor(4)
 
-            return self._parse_class()
+            return A3Class(**self._parse_class())
         elif is_identifier(char):
-            return self._parse_variable(char)
+            return A3Property(**self._parse_variable(char))
         
-        raise InvalidCharacterException(expected=['class', 'variable'], got=char + self._get(1))
+        raise InvalidCharacterException(expected=['class', 'variable'], got=char)
 
     def parse(self):
         try:
